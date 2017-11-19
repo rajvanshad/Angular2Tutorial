58a449d278d118a95ba71559b73033972af66126
Madhu K <rajvanshad@gmail.com>
2017-11-18 20:20:06 -0500

    Till Custom Pipe tutorial

--- app.module.ts
+++ app.module.ts
@@ -4,10 +4,11 @@ import {FormsModule} from '@angular/forms'
 import { AppComponent } from './app.component';
 import { EmployeeComponent } from './employee/employee.component'
 import {EmployeeListComponent} from './Employee/employeeList.component'
+import{EmployeeTitlePipe} from './Employee/employeeTitle.pipe'
 
 @NgModule({
   declarations: [
-    AppComponent, EmployeeComponent, EmployeeListComponent
+    AppComponent, EmployeeComponent, EmployeeListComponent, EmployeeTitlePipe
   ],
   imports: [
     BrowserModule, FormsModule
--- employee/employeeList.component.ts
+++ employee/employeeList.component.ts
@@ -12,10 +12,10 @@ export class EmployeeListComponent{
 
     constructor(){
     this.employees = [
-    {code:'emp101', name:'Tom', Gender: 'Male', annualSalary: 100000, dateOfBirth: '11/11/2010'},
-    {code:'emp102', name:'Jane', Gender: 'Female', annualSalary: 90000, dateOfBirth: '11/11/2011'},
-    {code:'emp103', name:'John', Gender: 'Male', annualSalary: 80000, dateOfBirth: '11/11/2012'},
-    {code:'emp104', name:'Mimi', Gender: 'Female', annualSalary: 70000, dateOfBirth: '11/11/2013'}
+    {code:'emp101', name:'Tom', gender: 'Male', annualSalary: 100000, dateOfBirth: '11/11/2010'},
+    {code:'emp102', name:'Jane', gender: 'Female', annualSalary: 90000, dateOfBirth: '11/11/2011'},
+    {code:'emp103', name:'John', gender: 'Male', annualSalary: 80000, dateOfBirth: '11/11/2012'},
+    {code:'emp104', name:'Mimi', gender: 'Female', annualSalary: 70000, dateOfBirth: '11/11/2013'}
     ];
 
     }
@@ -23,11 +23,11 @@ export class EmployeeListComponent{
     getEmployees(): void{
 
         this.employees = [
-            {code:'emp101', name:'Tom', Gender: 'Male', annualSalary: 100000, dateOfBirth: '11/11/2010'},
-            {code:'emp102', name:'Jane', Gender: 'Female', annualSalary: 90000, dateOfBirth: '11/11/2011'},
-            {code:'emp103', name:'John', Gender: 'Male', annualSalary: 80000, dateOfBirth: '11/11/2012'},
-            {code:'emp104', name:'Mimi', Gender: 'Female', annualSalary: 70000, dateOfBirth: '11/11/2013'},
-            {code:'emp105', name:'Nancy', Gender: 'Female', annualSalary: 60000, dateOfBirth: '11/11/2013'}
+            {code:'emp101', name:'Tom', gender: 'Male', annualSalary: 100000, dateOfBirth: '11/11/2010'},
+            {code:'emp102', name:'Jane', gender: 'Female', annualSalary: 90000, dateOfBirth: '11/11/2011'},
+            {code:'emp103', name:'John', gender: 'Male', annualSalary: 80000, dateOfBirth: '11/11/2012'},
+            {code:'emp104', name:'Mimi', gender: 'Female', annualSalary: 70000, dateOfBirth: '11/11/2013'},
+            {code:'emp105', name:'Nancy', gender: 'Female', annualSalary: 60000, dateOfBirth: '11/11/2013'}
             ];
           
     }
